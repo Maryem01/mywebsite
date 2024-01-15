d08eb1cc81dcecf107d6c6990afac65353292f6b
Maryem01 <maryemhaydar01@gmail.com>
2024-01-15 14:06:01 +0100

    english

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,8 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import resumepdf from "../../Assets/Sionx2.pdf";
-import resumepdf2 from "../../Assets/Sio.pdf";
+import pdf from "../../Assets/Mar.pdf";
+import resumepdf2 from "../../Assets/parttwo.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -15,10 +16,10 @@ function ResumeNew() {
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
- const handleDownload = () => {
-   window.open(resumepdf, "_blank");
-   window.open(resumepdf2, "_blank");
- };
+  const handleDownload = () => {
+    window.open(pdf, "_blank");
+    window.open(resumepdf2, "_blank");
+  };
   return (
     <div>
       <Container fluid className="resume-section">
@@ -35,7 +36,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumepdf} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
           <Document file={resumepdf2} className="d-flex justify-content-center">
